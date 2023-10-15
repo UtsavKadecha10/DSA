ceae041abd2ab2f1639b202d6092da4499cf262d
Utsav <ut.kadecha@gmail.com>
2023-10-15 12:29:12 +0530

    new

--- Lecture5_6_7/binaryToDecimal.cpp
+++ Lecture5_6_7/binaryToDecimal.cpp
@@ -1,10 +1,23 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
-int binaryToDecimal(){
-    
+int binaryToDecimal(int n){
+    int i=0;
+    int ans=0;
+    while(n!=0){   
+        int digit=n%10;
+        if(digit==1){
+            ans = ans + pow(2, i);
+        }   
+        n=n/10;
+        i++;
+    }
+    return ans;
 }
 int main(){
-
+    int n=10101;  
+    int result = binaryToDecimal(n);
+    cout<<result;
     return 0;
 }
---
+++ Lecture5_6_7/negativeDecimalToBinary.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std; 
+
+int negativeDecimalToBinary(int n){
+    int i=0;
+    int ans=0;
+    int bit;
+    while(n!=0){
+        
+    }
+}
+
+int main(){
+    int n=-6;
+    int result = negativeDecimalToBinary(n);
+    cout<<result;
+}
