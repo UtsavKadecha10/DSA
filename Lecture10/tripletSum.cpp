3bdfc56ff318b1c4d8dfb13aaab25f5e7239e8bf
Utsav <ut.kadecha@gmail.com>
2024-01-14 23:22:01 +0530

    14th jan commit

--- Lecture10/tripletSum.cpp
+++ Lecture10/tripletSum.cpp
@@ -2,3 +2,11 @@
 Also known as 3Sum
 https://www.codingninjas.com/studio/problems/triplet-with-given-sum_893028
 */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"triplet sum";
+    return 0;
+}
