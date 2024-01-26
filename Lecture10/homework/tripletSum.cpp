bf464e32978bbf758b8d91781cb357c6d5d31a34
Utsav <ut.kadecha@gmail.com>
2024-01-26 23:16:43 +0530

    26th jan commit

--- Lecture10/homework/tripletSum.cpp
+++ Lecture10/homework/tripletSum.cpp
@@ -5,9 +5,34 @@ https://www.codingninjas.com/studio/problems/triplet-with-given-sum_893028
 */
 
 #include<iostream>
+#include <bits/stdc++.h> 
 using namespace std;
-int main()
-{
+
+vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
+	set<vector<int>>visited;
+	vector<vector<int>>ans;
+    for (int i=0; i<n-2; i++){
+        for (int j=i+1; j<n-1; j++){
+            for (int k=j+1; k<n; k++){
+                // If we find a valid triplet.
+                if (arr[i]+arr[j]+arr[k]==K) {
+                    vector<int> triplet;
+                    triplet.push_back(arr[i]);
+                    triplet.push_back(arr[j]);
+                    triplet.push_back(arr[k]);
+                    // Sorting the triplet track distinct triplets.
+                    sort(triplet.begin(), triplet.end());
+                    if (visited.find(triplet) == visited.end()) {
+                        ans.push_back(triplet);
+                        visited.insert(triplet);
+                    }
+                }
+            }
+        }
+    }
+    return ans;
+}
+int main(){
     
     return 0;
 }
